4abf378abcb9ba6e5e6ce0ca8447ae73947487c6
miki honda <airis0314@gmail.com>
2017-07-16 06:07:06 +0000

    followings fix

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,7 +4,7 @@ class Micropost < ApplicationRecord
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 255 }
   
-  # has_many :reverses_of_favorite, class_name: 'Favorite', foreign_key: 'user_id'
+  # has_many :reverses_of_favorite, class_name: 'Favorite', foreign_key: 'micropost_id'
   # has_many :favorite_users, through: :reverses_of_favorite, source: :user
   
   has_many :favorites, dependent: :destroy
